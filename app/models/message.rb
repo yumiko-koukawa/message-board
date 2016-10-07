73a1dcaaf52acbfb86ebc9f1d940e51be8a109ba
Yumiko Kohkawa <sa136002@bbt.ohmae.ac.jp>
2016-10-07 11:07:03 +0000

    Message-Board Challenge2 再提出用

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20, :too_long => "最大20字以内" } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30, :too_long => "最大30字以内" } , presence: true
-  #年齢は0i以上の整数のみで３桁以内
-  validates :age , :numericality => { :only_integer => true, :greater_than => 0, :message => "半角数字のみ入力できます" }, length: { maximum: 3, :too_long => "3桁以内" } ,presence: true
+  #年齢は0歳以上の整数のみで３桁以内
+  validates :age , :numericality => { :only_integer => true,:greater_than_or_equal_to  => 0, :message => "半角数字のみ入力できます" }, length: { maximum: 3, :too_long => "3桁以内" } ,presence: true
 end
